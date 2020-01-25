9a0cd62e37ecb1d796104b6c17298535af21ac96
Kyle Miller <kylemiller3@my.unt.edu>
2020-01-25 05:33:23 -0500

    Fixed formatting issue

--- src/database.ts
+++ src/database.ts
@@ -5,7 +5,7 @@ import { async, } from 'rxjs/internal/scheduler/async';
 import { Utils, } from './utils';
 import { Event, } from './event';
 import { Settings, } from './settings';
-import { dbPassword } from './auth';
+import { dbPassword, } from './auth';
 
 // eslint-disable-next-line @typescript-eslint/no-namespace
 export namespace Db {
