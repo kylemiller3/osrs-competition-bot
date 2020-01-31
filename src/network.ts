1487b17b87778e64bd8e7276a2aa1a1c260043e9
Kyle Miller <kylemiller3@my.unt.edu>
2020-01-30 20:10:19 -0500

    Still quickfixing networking

--- src/network.ts
+++ src/network.ts
@@ -61,106 +61,104 @@ export namespace Network {
         );
         return ret;
     };
-    return deferred;
-};
 
-const CACHE_SIZE = 1000;
+    const CACHE_SIZE = 1000;
 
-/**
- * Interface describing the Hiscore Cache
- * @category Hiscore Cache
- * @ignore
- */
-interface HiscoreCache {
-    observable: Observable<hiscores.Player | null>
-    date: Date
-}
-
-/**
- * Implementation of the [[HiscoreCache]]
- * @category Hiscore Cache
- * @ignore
- */
-const hiscoreCache: Record<string, HiscoreCache | undefined> = {};
-
-/**
-* Fetches the supplied rsn from RuneScape API hiscores or cache.
-* Cache invalidates every 5 minutes. See [[hiscores.Player]]
-* @param rsn The rsn to lookup on hiscores
-* @param pullNew Forces a cache miss
-* @returns Observable of the RuneScape web API response
-* @category RuneScape API
-*/
-export const hiscoresFetch$ = (
-    rsn: string,
-    pullNew: boolean
-): Observable<hiscores.Player | null> => {
-    // eslint-disable-next-line no-control-regex
-    const asciiRsn: string = rsn.replace(/[^\x00-\x7F]/g, '');
-    Utils.logger.trace(`Looking up rsn '${asciiRsn}'`);
-    let cachedRsn = hiscoreCache[asciiRsn];
-    if (cachedRsn !== undefined) {
-        const date: Date = new Date(cachedRsn.date);
-        date.setMinutes(
-            date.getMinutes() + 5
-        );
-        if (Utils.isInPast(date) || pullNew) {
-            hiscoreCache[asciiRsn] = undefined;
-            cachedRsn = undefined;
-        }
+    /**
+     * Interface describing the Hiscore Cache
+     * @category Hiscore Cache
+     * @ignore
+    */
+    interface HiscoreCache {
+        observable: Observable<hiscores.Player | null>
+        date: Date
     }
 
-    if (cachedRsn === undefined) {
-        const bound = hiscores.getPlayer.bind(
-            undefined,
-            asciiRsn,
-        );
+    /**
+     * Implementation of the [[HiscoreCache]]
+     * @category Hiscore Cache
+     * @ignore
+    */
+    const hiscoreCache: Record<string, HiscoreCache | undefined> = {};
 
-        const isInputError: (error: Error) => boolean = (
-            error: Error
-        ): boolean => error.message === 'Player not found! Check RSN or game mode.'
-        || error.message === 'RSN must be less or equal to 12 characters'
-            || error.message === 'RSN must be of type string';
-        const obs = genericNetworkFetch$<hiscores.Player | null>(
-            bound,
-            (error: Error): boolean => {
-                if (isInputError(error)) {
-                    return false;
-                }
-                return true;
-            },
-        ).pipe(
-            catchError(
-                (error: Error): Observable<null> => {
-                    Utils.logger.error(`${error.name}`);
-                    if (isInputError(error)) {
-                        // consume this error
-                        // not a real networking error
-                        return of(null);
-                    }
-                    hiscoreCache[asciiRsn] = undefined;
-                    throw error;
-                }
-            ),
-            publishReplay(1),
-            refCount(),
-        );
-
-        hiscoreCache[asciiRsn] = {
-            observable: obs,
-            date: new Date(),
-        };
-
-        const keys = Object.keys(hiscoreCache);
-        if (keys.length >= CACHE_SIZE) {
-            const idxToRemove: number = Math.floor(
-                (Math.random() * CACHE_SIZE)
+    /**
+     * Fetches the supplied rsn from RuneScape API hiscores or cache.
+     * Cache invalidates every 5 minutes. See [[hiscores.Player]]
+     * @param rsn The rsn to lookup on hiscores
+     * @param pullNew Forces a cache miss
+     * @returns Observable of the RuneScape web API response
+     * @category RuneScape API
+    */
+    export const hiscoresFetch$ = (
+        rsn: string,
+        pullNew: boolean
+    ): Observable<hiscores.Player | null> => {
+        // eslint-disable-next-line no-control-regex
+        const asciiRsn: string = rsn.replace(/[^\x00-\x7F]/g, '');
+        Utils.logger.trace(`Looking up rsn '${asciiRsn}'`);
+        let cachedRsn = hiscoreCache[asciiRsn];
+        if (cachedRsn !== undefined) {
+            const date: Date = new Date(cachedRsn.date);
+            date.setMinutes(
+                date.getMinutes() + 5
             );
-            const keyToRemove: string = keys[idxToRemove];
-            hiscoreCache[keyToRemove] = undefined;
+            if (Utils.isInPast(date) || pullNew) {
+                hiscoreCache[asciiRsn] = undefined;
+                cachedRsn = undefined;
+            }
         }
-        return obs;
-    }
-    return cachedRsn.observable;
-};
+
+        if (cachedRsn === undefined) {
+            const bound = hiscores.getPlayer.bind(
+                undefined,
+                asciiRsn,
+            );
+
+            const isInputError: (error: Error) => boolean = (
+                error: Error
+            ): boolean => error.message === 'Player not found! Check RSN or game mode.'
+            || error.message === 'RSN must be less or equal to 12 characters'
+                || error.message === 'RSN must be of type string';
+            const obs = genericNetworkFetch$<hiscores.Player | null>(
+                bound,
+                (error: Error): boolean => {
+                    if (isInputError(error)) {
+                        return false;
+                    }
+                    return true;
+                },
+            ).pipe(
+                catchError(
+                    (error: Error): Observable<null> => {
+                        Utils.logger.error(`${error.name}`);
+                        if (isInputError(error)) {
+                            // consume this error
+                            // not a real networking error
+                            return of(null);
+                        }
+                        hiscoreCache[asciiRsn] = undefined;
+                        throw error;
+                    }
+                ),
+                publishReplay(1),
+                refCount(),
+            );
+
+            hiscoreCache[asciiRsn] = {
+                observable: obs,
+                date: new Date(),
+            };
+
+            const keys = Object.keys(hiscoreCache);
+            if (keys.length >= CACHE_SIZE) {
+                const idxToRemove: number = Math.floor(
+                    (Math.random() * CACHE_SIZE)
+                );
+                const keyToRemove: string = keys[idxToRemove];
+                hiscoreCache[keyToRemove] = undefined;
+            }
+            return obs;
+        }
+        return cachedRsn.observable;
+    };
 }
